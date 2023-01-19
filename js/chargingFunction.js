c372d5b874a10837be707300c7aa7d1568a5c03d
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-01-19 12:41:37 +0000

    datatable function add

---
+++ js/chargingFunction.js
@@ -0,0 +1,25 @@
+/**
+* @author ezekiel kouassi
+* @description : cette fonction va nous permettre de charger notre dataTable avec les données qui sont
+* contenu dans notre json
+*/
+
+(function loadDatatable() {
+
+    $('#data-table').DataTable({
+        "serverSide" : false,
+        "responsive" : true,
+        "autoWidth" : false,
+        "ajax" : {
+            "url" : "../jsonFile/datatable.json",
+            "dataSrc" : "data"
+        },
+        "columns" : [
+            {"data" : "number"},
+            {"data" : "type"},
+            {"data" : "itineraire"},
+            {"data" : "price"},
+            {"data" : "option"}
+        ]
+    })
+})()
